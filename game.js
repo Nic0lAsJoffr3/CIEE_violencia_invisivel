d9f39907df1019d925ed6c05f2634c9d3bfb1702
Nicolas joffre <103389503+Nic0lAsJoffr3@users.noreply.github.com>
2026-09-17 10:25:23 -0300

    Atualizar o game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://uijwislecgpeoyhucfxq.supabase.co";
-const SUPABASE_KEY = "sb_publishable_-JZE536NomschXIeZO6HNQ_VI08wmXP";
+const SUPABASE_KEY ="sb_publishable_JSPv7T4sd0K0z1yOYPQQjQ_qcFCWyAy";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
